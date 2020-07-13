95e8c2b9f09f7e8199692ad0280a5a941aab743d
yy <912916109@qq.com>
2020-07-13 22:46:36 +0800

    spring cloud v0.3 Eurake添加与注册

--- cloud-eureka-server-7001/src/main/java/com/cloud/yy/EurekaServer.java
+++ cloud-eureka-server-7001/src/main/java/com/cloud/yy/EurekaServer.java
@@ -2,12 +2,12 @@ package com.cloud.yy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain {
+@EnableEurekaServer
+public class EurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class,args);
+        SpringApplication.run(EurekaServer.class,args);
     }
 }
